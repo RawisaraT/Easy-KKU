89544e0116094590f9ab44fd8847f772b3c05b26
Rawisara <rawisara@kkumail.com>
2016-11-12 12:04:43 +0700

    Intent to Sign Up finish

--- app/src/main/java/kku/rawisarat/easykku/MainActivity.java
+++ app/src/main/java/kku/rawisarat/easykku/MainActivity.java
@@ -1,13 +1,36 @@
 package kku.rawisarat.easykku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //Explicit
+    private Button signInButton, signUpButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bilnd Widget
+
+        signInButton = (Button) findViewById(R.id.button);
+        signUpButton = (Button) findViewById(R.id.button2);
+
+        //Sign Up Controller
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SignUpActivity.class));
+            }
+        });
+
+    }   // Main method
+
+} //Main Class นี่คือ Class หลัก
