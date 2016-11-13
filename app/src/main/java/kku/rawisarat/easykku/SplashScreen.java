b5cc0f6c8fce96998e7eee6f37179f7b774bedd6
Rawisara <rawisara@kkumail.com>
2016-11-13 15:19:55 +0700

    finsh Project Easy KKU 13nov Fighting !!!

--- app/src/main/java/kku/rawisarat/easykku/SplashScreen.java
+++ app/src/main/java/kku/rawisarat/easykku/SplashScreen.java
@@ -1,8 +1,31 @@
 package kku.rawisarat.easykku;
 
-/**
- * Created by Koony on 13/11/2559.
- */
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class SplashScreen {
-}
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //Sound Effect
+        MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.intro_tata);
+        mediaPlayer.start();
+
+        //Auto Intent
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                finish();
+            }
+        },9000);
+
+    } // Main Method
+} // Main Class
